4160a688475819e462f3b1e8de78f7f7465fb8b1
skySungw <958408111@qq.com>
2021-04-14 20:00:42 +0800

    自动刷评论

--- tests/test.js
+++ tests/test.js
@@ -20,7 +20,23 @@ const { chromium } = require('playwright');
   const browser = await chromium.launch({headless: false, slowMo: 50 });
   const context = await browser.newContext({ acceptDownloads: true });
   const page = await context.newPage();
-  await page.goto("https://www.baidu.com");
-  await page.type('#kw', 'hello world');
-  await page.click('#su');
+  await page.goto("https://juejin.cn/post/6950227735830593566");
+  await page.click('text="登录"');
+  await page.fill('input[name="mobile"]', '15330265161');
+  await page.click('.send-vcode-btn');
+  setTimeout(async () => {
+    await page.type('.rich-input', '好文啊');
+    await page.click('.submit-btn');
+    let timer = null;
+    let count = 0;
+    timer = setInterval(async () => {
+      if (count++ > 10) {
+        clearInterval(timer);
+        timer = null;
+        return false;
+      }
+      await page.type('.rich-input', '好文啊');
+      await page.click('.submit-btn');
+    }, 3000)
+  }, 60 * 1000 * 2);
 })();
